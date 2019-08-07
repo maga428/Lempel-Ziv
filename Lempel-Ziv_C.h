d2557a8dcf4d89db8b596ad3b6d3526345d0baba
Komai <g2118007c0@edu.teu.ac.jp>
2019-08-07 13:17:59 +0900

    main.c, ヘッダー, LZC.c ファイルを作成

---
+++ Lempel-Ziv_C.h
@@ -0,0 +1,9 @@
+/*
+2019/08/07
+Lempel_Ziv complexity
+Lempel-Ziv_C.h
+*/
+#include<stdio.h>
+#include<stdlib.h>
+
+//Func
---
+++ Lempel-Ziv_Complexity.c
@@ -0,0 +1,5 @@
+/*
+2019/08/07
+Lempel_Ziv complexity
+Lempel-Ziv_Complexity.c
+*/
